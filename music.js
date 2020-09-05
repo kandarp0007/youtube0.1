50359f2809c7d9bfda7f4e243b3c514a002fbd45
kandarp <68642362+kandarp0007@users.noreply.github.com>
2020-09-05 22:52:02 +0530

    Add files via upload

---
+++ music.js
@@ -0,0 +1,19 @@
+audioPlayer();
+function audioPlayer(){
+  var currentSong = 0;
+  $("#audioPlayer")[0].src = $("#playlist li a")[0];
+  $("#audioPlayer")[0].play();
+  $("#playlist li a").click(function(e){
+     e.preventDefault(); 
+     $("#audioPlayer")[0].src = this;
+     $("#audioPlayer")[0].play();
+  });
+  
+  $("#audioPlayer")[0].addEventListener("ended", function(){
+     currentSong++;
+      if(currentSong == $("#playlist li a").length)
+          currentSong = 0;
+      $("#audioPlayer")[0].src = $("#playlist li a")[currentSong].href;
+      $("#audioPlayer")[0].play();
+  });
+}
